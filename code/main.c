0f54e42137b7ab22e127f953138cfbf2f878f497
silpasnr <silpasnr@gmail.com>
2012-04-08 12:52:46 +0530

    added extra function

--- code/main.c
+++ code/main.c
@@ -1,3 +1,4 @@
+#include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -10,16 +11,10 @@
 
 /* -MAIN- */
 int main (int argc, char *argv[]) {
-	char input [100];
-	int inputNum;
-	while (1) {
-		printf ("VManager # ");
-		scanf("%s", input);
-		if (!strcmp (input, "quit")) 
-			return 0;
-		inputNum = assignNum (input);
-		handleInput (inputNum);
-	}
-	return 0;
+	gtk_init (&argc, &argv);
+	
+    	createwidgets();
+
+    	return(0);
 }
 
